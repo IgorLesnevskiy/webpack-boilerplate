f2f05c863d842a9e04feda2f11c5dd9db8bcd037
Igor Lesnevskiy <lesnevskii.igor@gmail.com>
2020-01-16 09:47:01 +0300

    [i] add mock deploy config

---
+++ deploy.config.mock.js
@@ -0,0 +1,10 @@
+module.exports = {
+	user: "login",
+	password: "password",
+	host: "host",
+	port: 0,
+	include: ['**/*'],
+	localRoot: __dirname + "/dist",
+	remoteRoot: "/www/my_site",
+	deleteRemote: true
+};
